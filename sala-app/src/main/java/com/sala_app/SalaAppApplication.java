b0b6a14fac741c6e4620cc84b7693672dcf8cf9b
Adil-ha <hanoun.adil@gmail.com>
2025-05-01 09:06:07 +0200

    first commit

---
+++ sala-app/src/main/java/com/sala_app/SalaAppApplication.java
@@ -0,0 +1,13 @@
+package com.sala_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaAppApplication.class, args);
+	}
+
+}
